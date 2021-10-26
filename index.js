752f815c95577f17b44a8b92a18df0b12176d9aa
Nafis-Hasnat369 <nafis7823@gmail.com>
2021-10-27 01:26:56 +0600

    bug fixed

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const run = async () => {
         app.get('/services', async (req, res) => {
             const services = await servicesCollection.find({}).toArray();
             res.send(services);
-        })
+        });
 
         // Get Single Service
         app.get('/services/:id', async (req, res) => {
@@ -33,15 +33,13 @@ const run = async () => {
             const query = { _id: ObjectId(id) }
             const service = servicesCollection.findOne(query);
             res.json(service);
-        })
+        });
 
         // POST API
         app.post('/services', async (req, res) => {
             const service = req.body;
-            // console.log("Hit the post api", service)
             const result = await servicesCollection.insertOne(service);
-            console.log(result);
-            res.json(result)
+            res.json(result);
         });
 
         // DELETE API
@@ -50,13 +48,12 @@ const run = async () => {
             const query = { _id: ObjectId(id) }
             const result = await servicesCollection.deleteOne(query);
             res.json(result);
-
-        })
+        });
     }
     finally {
         // await client.close();
     }
-}
+};
 
 run().catch(console.dir);
 
